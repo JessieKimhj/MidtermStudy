35c54af03c25d3956371f05902a6a752193b33be
HyunJin Kim <dpro5893@gmail.com>
2023-10-18 20:50:59 -0700

    adding music project

---
+++ Music_Album_Vite_Project/music_album_vite_project/src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+import Player from './components/Player'
+
+function App() {
+
+  return (
+    <>
+    <Player />
+    </>
+  )
+}
+
+export default App
---
+++ Music_Album_Vite_Project/music_album_vite_project/src/components/Album.jsx
@@ -0,0 +1,27 @@
+export default function Album({id, name, coverImg, tracks, handleAlbumClick}) {
+    return (
+      <>
+      <section className="albums" style={{display: 'flex'}}>
+        <div>
+          <img
+            className="coverImg"
+            src={coverImg}
+            alt={name}
+            width={50}
+            height={50}
+            style={{borderRadius: '50%'}}
+          />
+        </div>
+        <div>  
+          <ul style={{listStyle: 'none', textDecoration:'none'}}>
+            <li>
+              <a href="#" onClick={() => handleAlbumClick({ id, name, coverImg, tracks })}>
+                {name}
+              </a>
+            </li>
+          </ul> 
+        </div>
+      </section>
+      </>
+    )
+  }
